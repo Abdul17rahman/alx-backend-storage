def3fa372438e37d9f2f841215ae51d86fcbbd8a
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-25 22:22:47 +0300

    fix count_call

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ def count_calls(method: Callable) -> Callable:
 	""" Decorator to count calls to Cache methods"""
 	@functools.wraps(method)
 	def wrapper(self, *args, **kwargs):
-		key = f"{method.__qualname__}"
+		key = method.__qualname__
 		self._redis.incr(key)
 		return method(self, *args, **kwargs)
 	return wrapper
