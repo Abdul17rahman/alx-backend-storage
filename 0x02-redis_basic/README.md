ac103cfebff55d62036e5a2dafe2486eb78e8203
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-25 20:18:50 +0300

    cache class

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,3 @@
+# 0x02-redis_basic
+
+Redis is a caching data storage or database.
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Cache class for redis database
+"""
+
+import redis
+import uuid as v4
+
+class Cache:
+	def __init__(self):
+		self._redis = redis.Redis()
+		self._redis.flushall()
+
+	def store(self, data):
+		"""
+		Stores the passed in data using uuid as keys
+		"""
+		key = str(v4.uuid4())
+		self._redis.set(key, data)
+		return key
